1f27cdf6b7a6dceec10bfe3862334c76f55cc15e
YU <dbcksals555@naver.com>
2023-06-21 16:54:12 +0900

    FirsttCommit

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import carimg from "./car13.png";
+function App() {
+  return (
+    <div className="App">
+      <img alt="" src={carimg} width={300} border="1"/>
+      <br/><br/>
+      <h2>Docker 배포 가즈아 ! ! !</h2>
+    </div>
+  );
+}
+
+export default App;
