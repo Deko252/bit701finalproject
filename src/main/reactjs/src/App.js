a0afc00c559747cf13160f1ab591a14d700672d6
YU <dbcksals555@naver.com>
2023-06-22 17:11:12 +0900

    그래들 수정 yml 수정 react 깃 연결 완료

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -6,7 +6,7 @@ function App() {
       <img alt="" src={carimg} width={300} border="1"/>
       <br/><br/>
       <h2>Docker 배포 가즈아 ! !</h2>
-        <div> JJ !!</div>
+        <div> JJ</div>
     </div>
   );
 }
